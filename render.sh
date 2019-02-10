4563f6b4268ca573547dc324862e8aa2f3e83cd2
Rebecca Turner <637275@gmail.com>
2019-02-10 14:12:10 -0500

    unquiet latexmk, force processing timesheet.tex

--- render.sh
+++ render.sh
@@ -5,5 +5,6 @@ set -v
 git pull --no-edit > /dev/null
 python3.7 -m pip install --user -r requirements.txt
 python3.7 timesheet.py > timesheet.tex
-latexmk -xelatex -halt-on-error -pvc- -pv- -quiet timesheet.tex
+rm -f timesheet.pdf
+latexmk -xelatex -halt-on-error -pvc- -pv- timesheet.tex
 latexmk -norc -c
