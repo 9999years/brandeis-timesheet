358a90da696cc577e17339b4687bc1541044fafd
Rebecca Turner <637275@gmail.com>
2019-03-13 16:04:58 -0400

    better render script for tracking local changes to settings.ini with a patch file

--- render.sh
+++ render.sh
@@ -2,9 +2,19 @@
 set -e
 set -v
 
+# don't worry about overwriting
+git checkout settings.ini
 git pull --no-edit > /dev/null
+# apply local patch changes
+patch settings.ini settings.patch
+
+# update requirements
 python3.7 -m pip install --user -r requirements.txt
+
+# gen timesheet
 python3.7 timesheet.py > timesheet.tex
+
+# (re)render timesheet.pdf
 rm -f timesheet.pdf
 latexmk -xelatex -halt-on-error -pvc- -pv- timesheet.tex
 latexmk -norc -c
